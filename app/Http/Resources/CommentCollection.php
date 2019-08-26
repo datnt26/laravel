f65f5f47ddc2069e21f91269411ac8f702f52fe0
datnt26 <datnt.2507@gmail.com>
2019-08-26 10:49:54 +0700

    Eloquent: API Resources[Resource Collection]

---
+++ app/Http/Resources/CommentCollection.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class CommentCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+
+    /* 
+        Dùng resource collection cho phương thức Comment::all()
+        Nếu không dùng resource collection thì buộc phải sử dụng : return parent::toArray($request);
+    */
+    public function toArray($request)
+    {
+        return [
+            'data' => $this->collection,
+            'links' => [
+                'self' => 'link-value',
+            ],
+        ];
+    }
+}
