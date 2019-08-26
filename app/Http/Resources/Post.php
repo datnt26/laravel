14d7e43a95abfcabe7bc0293a2f14e88fd7433a9
datnt26 <datnt.2507@gmail.com>
2019-08-26 14:58:30 +0700

    Eloquent: API Resources[Conditional Attributes]

--- app/Http/Resources/Post.php
+++ app/Http/Resources/Post.php
@@ -13,13 +13,18 @@ class Post extends JsonResource
      * @param  \Illuminate\Http\Request  $request
      * @return array
      */
+
+    /*
+        Conditional Attributes
+        -> Đặt điều kiện cho các thuộc tính. Nếu true => thuộc tính sẽ được trả về ,ngược lại sẽ bị xóa bỏ
+    */
     public function toArray($request)
     {
         //return parent::toArray($request);
         return [
             'data' => [
                 'id' => $this->id,
-                'user_id' => $this->user_id,
+                'user_id' => $this->when($this->user_id < 3, $this->user_id), // Trường user_id được trả về nếu user_id < 3,ngược lại nó sẽ bị xóa bỏ
                 'content' => $this->content,
                 'created' => $this->created,
                 'comments' => new CommentCollection($this->comments)
