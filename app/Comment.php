a5da6b36055354a17dbebe92bdc7bef6deaeb120
datnt26 <datnt.2507@gmail.com>
2019-08-30 08:32:46 +0700

    find thread in laravel [get reply of comment]

--- app/Comment.php
+++ app/Comment.php
@@ -9,4 +9,8 @@ class Comment extends Model
     public function user() {
         return $this->belongsTo('App\User','userId');
     }
+	// get reply comment
+    public function children() {
+	    return $this->hasMany('App\Comment', 'parent_id', 'id');
+	}
 }
